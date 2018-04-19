ca2b83e18e62f78c813b48b1eb332c054290b901
blacksinx <tas001@alumnos.ucn.cl>
2018-04-18 23:09:59 -0300

    Added: parte final

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
+
 
 @Component({
     selector: 'app-signup',
@@ -6,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-    test : Date = new Date();
-
-    constructor() { }
+    test: Date = new Date();
+    datos: any = {};
+    constructor(private http: Http) {
+    }
 
     ngOnInit() {}
+    login(inputEmail: String, inputPass: String) {
+        const apiURL = 'http://localhost:8080/persona?rut=' + inputEmail;
+        // this.getData(apiURL);
+        this.getImages(apiURL);
+    }
+    getData(apiURL) {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.get(apiURL).map((res: Response) => res.json())
+    }
+    getImages(apiURL) {
+        this.getData(apiURL).subscribe(datos => {
+            console.log(datos);
+            this.datos = datos;
+        });
+    }
 }
